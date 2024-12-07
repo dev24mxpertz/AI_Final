5161a4f3632232cbe84bb8eb554b029132077536
dev24mxpertz <dev24.mxpertz@gmail.com>
2024-12-07 16:40:44 +0530

    first commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,11 +6,14 @@
 // import glbModel from "../3Dassets/LOGO_GLB_WITH_LINE.glb";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+// gsap.registerPlugin(ScrollTrigger);
+
 // const Layout = () => {
 //   const [loadingNumber, setLoadingNumber] = useState("00");
 //   const [isLoading, setIsLoading] = useState(true);
-
-//   const ModelRef = useRef();
+//   const [isViewPage, setisViewPage] = useState("Page1");
+//   const modelRef = useRef();
+//   const Page1ContentRef = useRef();
 
 //   useEffect(() => {
 //     const tl = gsap.timeline({
@@ -24,9 +27,7 @@
 //           duration: 1,
 //           ease: "expo.out",
 //           delay: 1,
-//           onComplete: () => {
-//             setIsLoading(false);
-//           },
+//           onComplete: () => setIsLoading(false),
 //         });
 //       },
 //     });
@@ -36,13 +37,32 @@
 
 //   useEffect(() => {
 //     if (!isLoading) {
-//       gsap.delayedCall(1, () => {
-//         gsap.timeline().to(ModelRef.current, {
-//           left: "71%",
-//           top: "71%",
-//           ease: "expo.out",
-//           duration: 2,
-//         });
+//       gsap.delayedCall(2, () => {
+//         gsap
+//           .timeline()
+//           .to(modelRef.current, {
+//             left: "73%",
+//             top: "71%",
+//             ease: "expo.out",
+//             duration: 2,
+//           })
+//           .to(
+//             Page1ContentRef.current,
+//             { x: 20, ease: "expo.out", duration: 2 } // Starting properties
+//           );
+//       });
+//     }
+//   }, [isLoading]);
+
+//   useEffect(() => {
+//     if (!isLoading && isViewPage === "Page1") {
+//       const page1timeline = gsap.timeline();
+//       console.log("complier setup in the structure correctly")
+//       page1timeline.to(".Page1Container", {
+//         x: 1000,
+//         height: "20%",
+//         opacity: 0,
+//         duration: 2,
 //       });
 //     }
 //   }, []);
@@ -55,6 +75,7 @@
 //         color: "white",
 //         roughness: 0.5,
 //       });
+
 //       scene.traverse((child) => {
 //         if (child.isMesh) {
 //           child.material = whiteMaterial;
@@ -73,15 +94,27 @@
 //         </div>
 //       ) : (
 //         <div className="Main_Page_Container">
-//           <div ref={ModelRef} className="Model1_container">
-//             <Canvas camera={{ position: [0, 0, 3] }}>
-//               <ambientLight intensity={1} />
-//               <pointLight position={[5, 5, 5]} intensity={2} />
-//               <mesh position={[0, 0, 0]}>
+//           <div className="Page1Container">
+//             <div ref={modelRef} className="Model1_container">
+//               <Canvas camera={{ position: [0, 0, 3] }}>
+//                 <ambientLight intensity={1} />
+//                 <pointLight position={[5, 5, 5]} intensity={2} />
 //                 <Model path={glbModel} />
-//               </mesh>
-//               <OrbitControls />
-//             </Canvas>
+//                 <OrbitControls />
+//               </Canvas>
+//             </div>
+//             <div ref={Page1ContentRef} className="Page1_content_container">
+//               <h1>Reinventing Challenges into Triumphs with Custom AI.</h1>
+//               <h3>
+//                 Reinvent the value chain, solve unique problems, and boost
+//                 productivity, efficiency, and profitability with Bespoke AI
+//                 solutions.
+//               </h3>
+//               <span>
+//                 <i className="bi bi-arrow-down-circle"></i>
+
+//               </span>
+//             </div>
 //           </div>
 //         </div>
 //       )}
@@ -99,14 +132,27 @@ import * as THREE from "three";
 import glbModel from "../3Dassets/LOGO_GLB_WITH_LINE.glb";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// Register GSAP plugin
 gsap.registerPlugin(ScrollTrigger);
 
 const Layout = () => {
   const [loadingNumber, setLoadingNumber] = useState("00");
   const [isLoading, setIsLoading] = useState(true);
+  const [isViewPage, setIsViewPage] = useState("Page1");
   const modelRef = useRef();
-  const Page1ContentRef =  useRef()
+  const page1ContentRef = useRef();
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  console.log(screenWidth, "******screenwidth");
+
+  //Code For Update Screen width on window resize
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     const tl = gsap.timeline({
@@ -128,24 +174,121 @@ const Layout = () => {
     tl.to({}, { duration: 3, progress: 1 });
   }, []);
 
-useEffect(() => {
-  if (!isLoading) {
-    gsap.delayedCall(2, () => {
-      gsap
-        .timeline()
-        .to(modelRef.current, {
-          left: "73%",
-          top: "71%",
-          ease: "expo.out",
-          duration: 2,
-        })
-        .to(
-          Page1ContentRef.current,
-          { x: 20  ,  ease: "expo.out", duration: 2}, // Starting properties
+  useEffect(() => {
+    if (!isLoading) {
+      if (screenWidth < 600) {
+        gsap.delayedCall(2, () => {
+          gsap
+            .timeline()
+            .to(modelRef.current, {
+              left: "50%",
+              top: "75%",
+              ease: "expo.out",
+              duration: 2,
+            })
+            .to(page1ContentRef.current, {
+              x: 0,
+              ease: "expo.out",
+              duration: 2,
+            });
+        });
+      } else if (screenWidth > 1024) {
+        gsap.delayedCall(2, () => {
+          gsap
+            .timeline()
+            .to(modelRef.current, {
+              left: "73%",
+              top: "71%",
+              ease: "expo.out",
+              duration: 2,
+            })
+            .to(page1ContentRef.current, {
+              x: 20,
+              ease: "expo.out",
+              duration: 2,
+            });
+        });
+      }
+    }
+  }, [isLoading]);
+
+  // useEffect(() => {
+  //   if (!isLoading && isViewPage === "Page1") {
+  //     let previousScrollY = window.scrollY;
+
+  //     const onScroll = () => {
+  //       console.log("-------------- on Scroll triggereed --------");
+  //       const currentScrollY = window.scrollY;
+  //       console.log(
+  //         currentScrollY,
+  //         "-------------------------- currentScrollY -----------",
+  //         previousScrollY,
+  //         " ----------previousScrollY ----"
+  //       );
+  //       if (currentScrollY > previousScrollY) {
+  //         const page1Timeline = gsap.timeline();
+  //         page1Timeline.to(".Page1Container", {
+  //           x: 1000,
+  //           height: "20%",
+  //           opacity: 0,
+  //           duration: 2,
+  //           onComplete: () => {
+  //             window.removeEventListener("scroll", onScroll);
+  //           },
+  //         });
+  //       }
+  //       previousScrollY = currentScrollY;
+  //     };
+  //     window.addEventListener("scroll", onScroll);
+  //     return () => {
+  //         console.log("Removing scroll listener");
+  //       window.removeEventListener("scroll", onScroll);
+  //     };
+  //   }
+  // }, [isLoading, isViewPage]);
+
+  useEffect(() => {
+    if (!isLoading && isViewPage === "Page1") {
+      let previousScrollY = window.scrollY;
+
+      const onScroll = () => {
+        console.log("Scroll event triggered");
+        const currentScrollY = window.scrollY;
+        console.log(
+          "Current ScrollY:",
+          currentScrollY,
+          "Previous ScrollY:",
+          previousScrollY
         );
-    });
-  }
-}, [isLoading]);
+
+        if (currentScrollY > previousScrollY) {
+          console.log("User scrolled down");
+
+          const page1Timeline = gsap.timeline();
+          page1Timeline.to(".Page1Container", {
+            x: 1000,
+            height: "20%",
+            opacity: 0,
+            duration: 2,
+            onComplete: () => {
+              window.removeEventListener("scroll", onScroll);
+            },
+          });
+        }
+
+        previousScrollY = currentScrollY;
+      };
+
+      console.log("Adding scroll listener");
+
+      window.addEventListener("scroll", onScroll);
+
+      return () => {
+        console.log("Removing scroll listener");
+        window.removeEventListener("scroll", onScroll);
+      };
+    }
+  }, [isLoading, isViewPage]);
 
   const Model = ({ path }) => {
     const { scene } = useGLTF(path);
@@ -174,24 +317,26 @@ useEffect(() => {
         </div>
       ) : (
         <div className="Main_Page_Container">
-          <div ref={modelRef} className="Model1_container">
-            <Canvas camera={{ position: [0, 0, 3] }}>
-              <ambientLight intensity={1} />
-              <pointLight position={[5, 5, 5]} intensity={2} />
-              <Model path={glbModel} />
-              <OrbitControls />
-            </Canvas>
-          </div>
-          <div ref={Page1ContentRef} className="Page1_content_container">
-            <h1>Reinventing Challenges into Triumphs with Custom AI.</h1>
-            <h3>
-              Reinvent the value chain, solve unique problems, and boost
-              productivity, efficiency, and profitability with Bespoke AI
-              solutions.{" "}
-            </h3>
-            <span>
-              <i className="bi bi-arrow-down-circle"></i>
-            </span>
+          <div className="Page1Container">
+            <div ref={modelRef} className="Model1_container">
+              <Canvas camera={{ position: [0, 0, 3] }}>
+                <ambientLight intensity={1} />
+                <pointLight position={[5, 5, 5]} intensity={2} />
+                <Model path={glbModel} />
+                <OrbitControls />
+              </Canvas>
+            </div>
+            <div ref={page1ContentRef} className="Page1_content_container">
+              <h1>Reinventing Challenges into Triumphs with Custom AI.</h1>
+              <h3>
+                Reinvent the value chain, solve unique problems, and boost
+                productivity, efficiency, and profitability with Bespoke AI
+                solutions.
+              </h3>
+              <span>
+                <i className="bi bi-arrow-down-circle"></i>
+              </span>
+            </div>
           </div>
         </div>
       )}
