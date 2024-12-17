ace2bee6889f27be40a8e41481f0565a44c128f1
dev24mxpertz <dev24.mxpertz@gmail.com>
2024-12-17 12:36:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
     loadingWindowAnimation
       .to(".Loading_overlaydiv", {
         width: "100%",
-        left:"50%",
+        left: "50%",
         transform: " translate(-50%, -50%)",
         opacity: 1,
         ease: "power2.in",
@@ -33,20 +33,24 @@ const App = () => {
         duration: 0.5,
         ease: "power2.out",
       })
-      .to(".Loading_overlaydiv", {
-        left: "100%",
-        transform: "translate(50%, -50%)",
-        width: "0%",
-        opacity:0,
-        ease: "power2.out",
-        duration: 0.5,
-        onComplete: () => {
-          gsap.to(".Layout_container", {
-            height: "100vh",
-            duration: 0.1,
-          });
+      .to(
+        ".Loading_overlaydiv",
+        {
+          left: "100%",
+          transform: "translate(50%, -50%)",
+          width: "0%",
+          opacity: 0,
+          ease: "power2.out",
+          duration: 0.5,
+          onComplete: () => {
+            gsap.to(".Layout_container", {
+              height: "100vh",
+              duration: 0.1,
+            });
+          },
         },
-      } , "-=0.5");
+        "-=0.5"
+      );
 
     const loadingNumberTimeline = gsap.timeline({
       onUpdate: () => {
@@ -126,13 +130,189 @@ const App = () => {
           </div>
         </div>
         <div className="scroll-section anim">
-          <div className="content">
-            <h1>Section 2</h1>
+          <div className="section2content">
+            <h1>
+              REINVENTION <br /> WITH GenAI
+            </h1>
+            <h3>
+              IS THE DEFAULT <br /> STRATEGY FOR SUCCESS
+            </h3>
+            <p>
+              The rate of change affecting businesses has risen over 183% in the
+              last 4 years. <br />
+              To counter this, 83% of organisations have accelerated the
+              execution of reinvention with GenAI.
+            </p>
           </div>
         </div>
         <div className="scroll-section anim">
-          <div className="content">
-            <h1>Section 3</h1>
+          <div className="section3content">
+            <h1>More than twice the revenue</h1>
+            <h3>in less than half the time against competitors</h3>
+            <div className="section3content_headingbox">
+              <h3>Greater improvements in productivity </h3>
+              <h3>Higher average revenue growth </h3>
+            </div>
+            <div className="section3content_headingbox">
+              <p>
+                3M HOURS <br /> SAVED
+                <br />
+                An agency saved 3 million operational <br />
+                hours using GenAI
+              </p>
+              <p>
+                3M HOURS <br />
+                SAVED <br />A company delivered 16 million <br />
+                personalised offerings to their customers within 3 months.
+              </p>
+              <p>
+                3M HOURS <br />
+                SAVED <br />
+                An insurer increased the potential <br />
+                revenue by 10% using AI in only one single <br />
+                function of the whole process
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* ---- */}
+        <div className="scroll-section anim">
+          <div className="section4content">
+            <h2>OUR SUITE OF SERVICES</h2>
+            <div className="section4content_container">
+              <div className="section4content_box">
+                <h3>A. AI ASSISTANT FOR CUSTOMER SERVICE</h3>
+                <p>
+                  Eliminate unique customer frustrations and operational issues
+                  to deliver a seamless experience that keeps them coming back,
+                  all while driving greater operational efficiency and
+                  productivity with the custom AI assistant for customer
+                  service.
+                </p>
+                <button>LEARN MORE</button>
+              </div>
+              <div className="section4content_box">
+                <h3>B. AI WORKFORCE FOR BUSINESS OPERATIONS </h3>
+                <p>
+                  Imagine a workplace where challenges are met head-on,
+                  departments thrive, and employees are empowered to focus on
+                  creativity and innovation—all driven by custom AI solutions
+                  applicable throughout the value chain, designed to solve your
+                  unique business problems and inefficiencies.
+                </p>
+                <button>LEARN MORE</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* -------------- */}
+        <div className="scroll-section anim">
+          <div className="section5content">
+            <h2>
+              ACHIEVE YOUR VISION AND MISSION with ALTUS INTELLIGENT SOLUTIONS{" "}
+            </h2>
+            <div className="section5content_container">
+              <div className="section5content_box">
+                <h3>BOOST YOUR SUCCESS</h3>
+                <p>
+                  Our goal is to help you eliminate obstacles and unlock your
+                  full potential.
+                </p>
+                <p>
+                  Our intelligent solutions target unique challenges, enhance
+                  customer experiences, and fuel innovation, setting you apart
+                  in a competitive landscape.
+                </p>
+              </div>
+              <div className="section5content_box">
+                <h3>MAX RESULTS, LOW OVERHEAD</h3>
+                <p>Achieve more without heavy costs!</p>
+                <p>
+                  By optimising processes, we help you achieve peak performance
+                  while keeping operational expenses in check.
+                </p>
+              </div>
+              <div className="section5content_box">
+                <h3>EMPOWERED HUMAN CAPITAL</h3>
+                <p>Empower your team to do their best work.</p>
+                <p>
+                  Ease frustrations, mitigate risks, and handle routine tasks,
+                  enabling your employees to focus on creativity, innovation,
+                  and high-value work—leading to a more engaged, productive, and
+                  motivated workforce.
+                </p>
+              </div>
+              <div className="section5content_box">
+                <h3>ADAPTIVE SCALABILITY</h3>
+                <p>AI Tools that grow with you.</p>
+                <p>
+                  We proactively ensure that your solutions meet new demands so
+                  as you grow, your processes and systems remain
+                  optimised—enabling confident and sustainable scaling.
+                </p>
+              </div>
+              <div className="section5content_box">
+                <h3>BE A DIFFERENTIATOR</h3>
+                <p>Stand out from the crowd.</p>
+                <p>
+                  Our customised solutions enable you to offer distinctive
+                  capabilities that set you apart from competitors.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* ---------------------- */}
+        <div className="scroll-section anim">
+          <div className="section6content">
+            <h2>Partner with us to achieve your vision and mission </h2>
+            <div className="section6content_container">
+              <div className="section6content_box">
+                <h3>BESPOKE SOLUTIONS</h3>
+                <h4>We solve your unique problems </h4>
+                <p>
+                  We collaborate closely with you to design and deliver
+                  customised solutions, ensuring that our AI tools are
+                  fine-tuned to address your unique business problems and
+                  requirements.
+                </p>
+              </div>
+              <div className="section6content_box">
+                <h3>PARTNERSHIP FOR LASTING IMPACT </h3>
+                <h4>Your success is our priority, now and in the future.</h4>
+                <p>
+                  We provide regular updates, ongoing support, and continuous
+                  optimisation to ensure your unique challenges are always
+                  addressed, and our AI solutions continue delivering value as
+                  your business grows.
+                </p>
+              </div>
+              <div className="section6content_box">
+                <h3>SEAMLESS INTEGRATION</h3>
+                <h4>Resolve business problems smoothly and effortlessly.</h4>
+                <p>
+                  Our solutions are designed to work harmoniously with your
+                  existing systems, minimising disruption and ensuring a quick
+                  and efficient transition.
+                </p>
+              </div>
+              <div className="section6content_box">
+                <h3>EXPERTISE</h3>
+                <h4>
+                  Years of experience, young creative minds, and passion for
+                  problem-solving and innovation.
+                </h4>
+                <p>
+                  With deep industry knowledge, a flare of creativity,
+                  experience, and a commitment to innovation, we don’t just
+                  offer technology—we partner and consult with you to unlock
+                  your business’s full potential by concisely identifying
+                  challenges, discerning room for improvements, and
+                  comprehending your unique requirements to develop superior
+                  solutions.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
